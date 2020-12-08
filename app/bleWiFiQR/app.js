2b8dddfb8707b028f9da3c5740a487ae6fd08593
tungedison <38431490+tungedison@users.noreply.github.com>
2020-12-08 11:27:30 +0700

    Update app.js

--- app/bleWiFiQR/app.js
+++ app/bleWiFiQR/app.js
@@ -17,11 +17,11 @@ var bluetoothDevice;
 var bleMsgLabel = document.getElementById("bleStateLabel");
 var setupMsgLabel = document.getElementById("setupStateLabel");
 
-var serviceUuid    = "6E400001-B5A3-F393-E0A9-E50E24DCCA9";
-var typeCharUuid   = "6E400004-B5A3-F393-E0A9-E50E24DCCA9";
-var ssidCharUuid   = "6E400002-B5A3-F393-E0A9-E50E24DCCA9";
-var passCharUuid   = "6E400003-B5A3-F393-E0A9-E50E24DCCA9";
-var actionCharUuid = "6E400005-B5A3-F393-E0A9-E50E24DCCA9";
+var serviceUuid    = "0000dd00-0000-1000-8000-00805f9b34fb";
+var typeCharUuid   = "0000dd01-0000-1000-8000-00805f9b34fb";
+var ssidCharUuid   = "0000dd02-0000-1000-8000-00805f9b34fb";
+var passCharUuid   = "0000dd03-0000-1000-8000-00805f9b34fb";
+var actionCharUuid = "0000dd05-0000-1000-8000-00805f9b34fb";
 
 var typeCharacteristic;
 var ssidCharacteristic;
